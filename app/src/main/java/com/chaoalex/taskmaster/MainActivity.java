407b2fab0b58f840a000cf2cb27751ba49e18009
AlexSaeChao <chaoalex93@gmail.com>
2023-08-16 22:43:21 -0700

    readme updated

--- app/src/main/java/com/chaoalex/taskmaster/MainActivity.java
+++ app/src/main/java/com/chaoalex/taskmaster/MainActivity.java
@@ -2,14 +2,17 @@ package com.chaoalex.taskmaster;
 
 import static com.chaoalex.taskmaster.activities.SettingsActivity.USER_NICKNAME_TAG;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -127,7 +130,18 @@ public class MainActivity extends AppCompatActivity {
 //    TaskListRecyclerViewAdapter adapter = new TaskListRecyclerViewAdapter(tasks);
 //    TaskListsRecyclerView.setAdapter(adapter);
 
-    int spaceInPixels = 8;
+    int spaceInPixels = getResources().getDimensionPixelSize(R.dimen.task_fragment_spacing);
+    TaskListsRecyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+      @Override
+      public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+        super.getItemOffsets(outRect, view, parent, state);
+        outRect.bottom = spaceInPixels;
+
+        if(parent.getChildAdapterPosition(view) == tasks.size()-1) {
+          outRect.bottom = 0;
+        }
+      }
+    });
 
     TaskListRecyclerViewAdapter adapter = new TaskListRecyclerViewAdapter(tasks, this);
 
